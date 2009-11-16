91740a8f6f05feaf404ab41a896385f558415bf7
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2009-11-16 20:26:21 +0000

    merged OSC files

--- trunk/setup.py
+++ trunk/setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup, Extension
 import os
 
 source_files = ["src/pyomodule.c", "src/servermodule.c", "src/streammodule.c", "src/tablemodule.c", "src/oscilmodule.c",
-                "src/biquadmodule.c", "src/noisemodule.c", "src/distomodule.c", "src/dummymodule.c",
-                "src/inputmodule.c", "src/fadermodule.c", "src/midictlmodule.c", "src/mixmodule.c", "src/oscsendmodule.c",
+                "src/filtremodule.c", "src/noisemodule.c", "src/distomodule.c", "src/dummymodule.c",
+                "src/inputmodule.c", "src/fadermodule.c", "src/midictlmodule.c", "src/mixmodule.c",
                 "src/oscreceivemodule.c"]
 include_dirs = ['include']
 libraries = ['portaudio', 'portmidi', 'sndfile', 'lo']
--- trunk/start.py
+++ trunk/start.py
@@ -10,7 +10,7 @@ import random
 
 s = Server(sr=44100, nchnls=2, buffersize=256, duplex=0)
 
-example = 2
+example = 12
 
 if example == 1:
     t = HarmTable([1,0,0,.2,0,0,.1,0,0,.04])
