93cb3ddec7be1737a7c1a1012b54921f5e07591e
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-02-10 19:26:50 +0000

    hierarchy clean up (final pass)

--- trunk/pyo.py
+++ trunk/pyo.py
@@ -1,14 +1,16 @@
 from distutils.sysconfig import get_python_lib
 import os
 
+import pyolib.analysis as analysis
 from pyolib.analysis import *
 from pyolib.controls import *
 from pyolib.dynamics import *
 from pyolib.effects import *
 from pyolib.filters import *
 from pyolib.generators import *
+#from pyolib.maths import *
 from pyolib.midi import *
-from pyolib.OSC import *
+from pyolib.opensoundcontrol import *
 from pyolib.pan import *
 from pyolib.pattern import *
 from pyolib.server import *
@@ -19,19 +21,20 @@ from pyolib.triggers import *
 
 OBJECTS_TREE = {'functions': sorted(['pa_count_devices', 'pa_get_default_input', 'pa_get_default_output', 'pa_list_devices', 
                     'pm_count_devices', 'pm_list_devices', 'sndinfo']),
-        'PyoObject': {'Filters': sorted(['Biquad', 'BandSplit', 'Port', 'Hilbert', 'Tone', 'DCBlock']),
-                      'Open Sound Control': sorted(['OscReceive', 'OscSend']),
-                      'Effects': sorted(['Delay', 'Disto', 'Freeverb', 'Waveguide']),
-                      'Triggers': sorted(['Metro', 'TrigEnv', 'TrigRand', 'Select', 'Counter', 'TrigChoice', 'TrigFunc']),
-                      'Generators': sorted(['Noise', 'Phasor', 'Sig', 'SigTo', 'Sine', 'Input', 'Fader']),
-                      'MIDI': sorted(['Midictl', 'Notein']),
-                      'Sound players': sorted(['SfMarkerShuffler', 'SfPlayer']),
-                      'Patterns': sorted(['Pattern']),
-                      'Panning': sorted(['Pan', 'SPan']),
-                      'Table process': sorted(['TableRec', 'Osc', 'Pointer', 'Granulator', 'Pulsar', 'TableRead']),
-                      'Analysis': sorted(['Follower']),
-                      'Dynamics': sorted(['Clip', 'Compress']),
-                      'Internal objects': sorted(['Dummy', 'InputFader', 'Mix'])},
+        'PyoObject': {'analysis': sorted(['Follower']),
+                      'controls': sorted(['Fader', 'Sig', 'SigTo']),
+                      'dynamics': sorted(['Clip', 'Compress']),
+                      'effects': sorted(['Delay', 'Disto', 'Freeverb', 'Waveguide']),
+                      'filters': sorted(['Biquad', 'BandSplit', 'Port', 'Hilbert', 'Tone', 'DCBlock']),
+                      'generators': sorted(['Noise', 'Phasor', 'Sine', 'Input']),
+                      'internal objects': sorted(['Dummy', 'InputFader', 'Mix']),
+                      'midi': sorted(['Midictl', 'Notein']),
+                      'opensoundcontrol': sorted(['OscReceive', 'OscSend']),
+                      'pan': sorted(['Pan', 'SPan']),
+                      'patterns': sorted(['Pattern']),
+                      'sfplayer': sorted(['SfMarkerShuffler', 'SfPlayer']),
+                      'tableprocess': sorted(['TableRec', 'Osc', 'Pointer', 'Granulator', 'Pulsar', 'TableRead']),
+                      'triggers': sorted(['Metro', 'TrigEnv', 'TrigRand', 'Select', 'Counter', 'TrigChoice', 'TrigFunc'])},
         'Map': {'SLMap': sorted(['SLMapFreq', 'SLMapMul', 'SLMapPhase', 'SLMapQ', 'SLMapDur', 'SLMapPan'])},
         'PyoTableObject': sorted(['LinTable', 'NewTable', 'SndTable', 'HannTable', 'HarmTable']),
         'Server': [], 
