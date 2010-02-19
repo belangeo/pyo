f0b4f0137399f6cc7adede097006de854e8ee657
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-02-19 02:44:41 +0000

    Added portamento to TrigRand

--- examples/granulation.py
+++ examples/granulation.py
@@ -3,10 +3,10 @@ s = Server(buffersize=512).boot()
 
 t = HarmTable([1,0,.3,0,.2])
 
-snd = SndTable(DEMOS_PATH + '/accord.aif')
+snd = SndTable('/Users/olipet/svn/pulsar/wavePulsar.aif')
 env = HannTable()
 
-pos = Phasor(snd.getRate()*.02, 0, snd.getSize())
+pos = Phasor(snd.getRate()*.01, 0, snd.getSize())
 pnz = Noise(5)
 
 met = Metro(.0001).play()
@@ -15,6 +15,6 @@ dur = Noise(.002, .1)
 
 gran = Granulator(table=snd, env=env, pitch=[.999, 1.0011], pos=pos+pnz, 
                   dur=dur*trs, grains=100, basedur=.1, mul=.01).out()
-
+         
 s.gui(locals())
 
