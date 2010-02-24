b3f4d793e96644b0e64f5da621179461606fca9c
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-02-24 21:00:56 +0000

    Sig's value attribute can be a PyoObject

--- examples/convolution.py
+++ examples/convolution.py
@@ -10,7 +10,6 @@ s = Server(sr=44100, nchnls=2, buffersize=512, duplex=1).boot()
 def sampsToMs(x):
     return x / s.getSamplingRate()
 
-#sf = SfPlayer('/Users/olipet/Desktop/sons/cacanne4.aiff', loop=True, speed=1, mul=.5)
 sf = Noise(.5)
 
 TLEN = 400
@@ -29,14 +28,11 @@ rec3 = TableRec(inmic, table=t3, fadetime=.001)
 t4 = NewTable(length=sampsToMs(TLEN), chnls=1)
 rec4 = TableRec(inmic, table=t4, fadetime=.001)
 
-
-maps = [SLMap(0,1,"lin","phase",0)]
-pha = Phasor(0, 0)
-pha.ctrl(maps)
-
+pha = Sig(0)
+pha.ctrl()
 
 t = NewTable(length=sampsToMs(TLEN), chnls=1)
-m = TableMorph(pha, t, [t1,t2])
+m = TableMorph(pha, t, [t1,t2,t3,t4])
 a = Convolve(sf, table=t, size=t.getSize()).out()
 
 s.gui(locals())
