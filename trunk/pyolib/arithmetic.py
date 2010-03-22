b4d107425e75c1ad940a7a02c0d910745ada8289
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-03-22 18:37:28 +0000

    added M_Tan

--- trunk/pyolib/arithmetic.py
+++ trunk/pyolib/arithmetic.py
@@ -15,7 +15,7 @@ class M_Sin(PyoObject):
     Parameters:
 
     input : PyoObject
-        Input signal to filter.
+        Input signal, angle in radians.
 
     Methods:
 
@@ -83,7 +83,7 @@ class M_Cos(PyoObject):
     Parameters:
 
     input : PyoObject
-        Input signal to filter.
+        Input signal, angle in radians.
 
     Methods:
 
@@ -109,6 +109,74 @@ class M_Cos(PyoObject):
         self._in_fader = InputFader(input)
         in_fader, mul, add, lmax = convertArgsToLists(self._in_fader, mul, add)
         self._base_objs = [M_Cos_base(wrap(in_fader,i), wrap(mul,i), wrap(add,i)) for i in range(lmax)]
+
+    def __dir__(self):
+        return ['input', 'mul', 'add']
+
+    def setInput(self, x, fadetime=0.05):
+        """
+        Replace the `input` attribute.
+
+        Parameters:
+
+        x : PyoObject
+            New signal to process.
+        fadetime : float, optional
+            Crossfade time between old and new input. Default to 0.05.
+
+        """
+        self._input = x
+        self._in_fader.setInput(x, fadetime)
+
+    def ctrl(self, map_list=None, title=None):
+        self._map_list = []
+        PyoObject.ctrl(self, map_list, title)
+
+    @property
+    def input(self):
+        """PyoObject. Input signal to filter.""" 
+        return self._input
+    @input.setter
+    def input(self, x): self.setInput(x)
+
+class M_Tan(PyoObject):
+    """
+    Performs a tangent function on audio signal.
+
+    Returns the tangent of input.
+
+    Parent class: PyoObject
+
+    Parameters:
+
+    input : PyoObject
+        Input signal, angle in radians.
+
+    Methods:
+
+    setInput(x, fadetime) : Replace the `input` attribute.
+
+    Attributes:
+
+    input : PyoObject. Input signal to filter.
+
+    Examples:
+
+    >>> s = Server(duplex=1).boot()
+    >>> s.start()
+    >>> t = HarmTable([1,0,.33,0,.2,0,.143,0,.111])
+    >>> a = Osc(t, 1, 0, .5, .5)
+    >>> b = M_Tan(a)
+    >>> c = Osc(t, 100, mul=b).out()
+
+    """
+    def __init__(self, input, mul=1, add=0):
+        self._input = input
+        self._mul = mul
+        self._add = add
+        self._in_fader = InputFader(input)
+        in_fader, mul, add, lmax = convertArgsToLists(self._in_fader, mul, add)
+        self._base_objs = [M_Tan_base(wrap(in_fader,i), wrap(mul,i), wrap(add,i)) for i in range(lmax)]
 
     def __dir__(self):
         return ['input', 'mul', 'add']
