60b8e72f37074768127b284d3b16d9464385747c
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-04-07 19:18:25 +0000

    work on get_dep_headers script

--- trunk/scripts/get_dep_headers.sh
+++ trunk/scripts/get_dep_headers.sh
@@ -1,6 +1,22 @@
-cd /usr/local/include
-mkdir lo
-cd lo
-sudo cp /Users/olipet/svn/pyo_dep_headers/lo/* .
-cd ..
-sudo cp /Users/olipet/svn/pyo_dep_headers/*.h .
+if cd /usr/local/include; 
+then 
+    if cd /usr/local/include/lo;
+    then
+        sudo scp 132.204.178.49:/Library/WebServer/Documents/pyo_dep_headers/lo/* .
+        cd ..;
+    else
+        sudo mkdir lo
+        cd lo 
+        sudo scp 132.204.178.49:/Library/WebServer/Documents/pyo_dep_headers/lo/* .;
+    fi    
+    
+else 
+    sudo mkdir /usr/local/include; 
+        
+fi
+
+
+#cd lo
+#sudo cp /Users/olipet/svn/pyo_dep_headers/lo/* .
+#cd ..
+#sudo cp /Users/olipet/svn/pyo_dep_headers/*.h .
