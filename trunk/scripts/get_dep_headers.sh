46ddae3b4b2850de8322713b1bbb4f4974ef271f
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-04-08 18:57:40 +0000

    removed dependencies headers from sources (added a script to get them)

--- trunk/scripts/get_dep_headers.sh
+++ trunk/scripts/get_dep_headers.sh
@@ -1,22 +1,27 @@
 if cd /usr/local/include; 
 then 
-    if cd /usr/local/include/lo;
-    then
-        sudo scp 132.204.178.49:/Library/WebServer/Documents/pyo_dep_headers/lo/* .
-        cd ..;
-    else
-        sudo mkdir lo
-        cd lo 
-        sudo scp 132.204.178.49:/Library/WebServer/Documents/pyo_dep_headers/lo/* .;
-    fi    
-    
+    pwd;
 else 
-    sudo mkdir /usr/local/include; 
-        
+    sudo mkdir /usr/local/include
+    cd /usr/local/include;
 fi
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/portaudio.h -o "portaudio.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/portmidi.h -o "portmidi.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/sndfile.h -o "sndfile.h"
 
 
-#cd lo
-#sudo cp /Users/olipet/svn/pyo_dep_headers/lo/* .
-#cd ..
-#sudo cp /Users/olipet/svn/pyo_dep_headers/*.h .
+if cd lo;
+then
+    pwd;
+else
+    sudo mkdir lo
+    cd lo;
+fi    
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_endian.h -o "lo_endian.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_errors.h -o "lo_errors.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_lowlevel.h -o "lo_lowlevel.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_macros.h -o "lo_macros.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_osc_types.h -o "lo_osc_types.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_throw.h -o "lo_throw.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_types.h -o "lo_types.h"
+sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo.h -o "lo.h"
