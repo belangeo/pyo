a7bcdeaead937062ec7fff2de581a13a6169cbc0
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-05-14 21:23:18 +0000

    added dylib to dependencies script

--- trunk/scripts/get_dep_headers.sh
+++ trunk/scripts/get_dep_headers.sh
@@ -25,3 +25,20 @@ sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_osc_types.h -o "lo_
 sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_throw.h -o "lo_throw.h"
 sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo_types.h -o "lo_types.h"
 sudo curl http://www.iact.umontreal.ca/pyo_dep_headers/lo/lo.h -o "lo.h"
+
+if cd /usr/local/lib; 
+then 
+    pwd;
+else 
+    sudo mkdir /usr/local/lib
+    cd /usr/local/lib;
+fi
+sudo curl http://www.iact.umontreal.ca/pyo_deps/liblo.0.6.0.dylib -o "liblo.0.6.0.dylib"
+sudo curl http://www.iact.umontreal.ca/pyo_deps/libportaudio.2.0.0.dylib -o "libportaudio.2.0.0.dylib"
+sudo curl http://www.iact.umontreal.ca/pyo_deps/libportmidi.dylib -o "libportmidi.dylib"
+sudo curl http://www.iact.umontreal.ca/pyo_deps/libsndfile.1.dylib -o "libsndfile.1.dylib"
+sudo ln -s libsndfile.1.dylib libsndfile.dylib
+sudo ln -s liblo.0.6.0.dylib liblo.dylib
+sudo ln -s liblo.0.6.0.dylib liblo.0.dylib
+sudo ln -s libportaudio.2.0.0.dylib libportaudio.dylib
+sudo ln -s libportaudio.2.0.0.dylib libportaudio.2.dylib
