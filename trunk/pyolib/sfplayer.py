65964f93f8482044c58ef8152f554cb23239f01e
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-07-01 18:01:29 +0000

    new object: SfMarkerLooper

--- trunk/pyolib/sfplayer.py
+++ trunk/pyolib/sfplayer.py
@@ -435,3 +435,195 @@ class SfMarkerShuffler(PyoObject):
         return self._interp
     @interp.setter
     def interp(self, x): self.setInterp(x)
+
+class SfMarkerLooper(PyoObject):
+    """
+    AIFF with markers soundfile looper.
+
+    Reads audio data from a AIFF file, and can alter its pitch using 
+    one of several available interpolation types, as well as convert 
+    the sample rate to match the Server sampling rate setting. The 
+    reading pointer loops a specific marker (set in the header of the 
+    file) until it received a new integer in the `mark` parameter.
+
+    Parent class: PyoObject
+
+    Parameters:
+
+    path : string
+        Full path name of the sound to read.
+    speed : float or PyoObject, optional
+        Transpose the pitch of input sound by this factor. 1 is the 
+        original pitch, lower values play sound slower, and higher 
+        values play sound faster. Negative values results in playing 
+        sound backward. At audio rate, fast changes between positive and
+        negative values can result in strong DC components in the output 
+        sound. Defaults to 1.
+    mark : float or PyoObject, optional
+        Integer denoting the marker to loop, in the range 0 -> len(getMarkers()).
+        Defaults to 0.
+    interp : int, optional
+        Choice of the interpolation method. Defaults to 2.
+            1 : no interpolation
+            2 : linear
+            3 : cosinus
+            4 : cubic
+
+    Methods:
+
+    setSpeed(x) : Replace the `speed` attribute.
+    setInterp(x) : Replace the `interp` attribute.
+    setMark(x) : Replace the `mark` attribute.
+    getMarkers() : Returns a list of marker time values in samples.
+
+    Attributes:
+
+    speed : float or PyoObject, Transposition factor.
+    mark : float or PyoObject, Marker to loop.
+    interp : int {1, 2, 3, 4}, Interpolation method.
+
+    Notes:
+
+    Reading backward with fast changes at audio rate can generates strong 
+    DC in the resulting sound.
+
+    Examples:
+
+    >>> s = Server().boot()
+    >>> s.start()
+    >>> a = SfMarkerLooper(SNDS_PATH + '/transparent.aif').out()
+    >>> rnd = RandInt(len(a.getMarkers()), 100)
+    >>> a.mark = rnd
+
+    """
+    def __init__(self, path, speed=1, mark=0, interp=2, mul=1, add=0):
+        PyoObject.__init__(self)
+        self._speed = speed
+        self._mark = mark
+        self._interp = interp
+        self._mul = mul
+        self._add = add
+        path, speed, mark, interp, mul, add, lmax = convertArgsToLists(path, speed, mark, interp, mul, add)
+        self._base_players = []
+        self._base_objs = []
+        self._snd_size, self._dur, self._snd_sr, self._snd_chnls = sndinfo(path[0])
+        for i in range(lmax):
+            try:
+                sf = aifc.open(wrap(path,i))
+                markerstmp = sf.getmarkers()
+                sf.close()
+                self._markers = [m[1] for m in markerstmp]
+            except:
+                self._markers = []    
+            self._base_players.append(SfMarkerLooper_base(wrap(path,i), self._markers, wrap(speed,i), wrap(mark,i), wrap(interp,i)))
+        for i in range(lmax * self._snd_chnls):
+            j = i / self._snd_chnls
+            self._base_objs.append(SfMarkerLoop_base(wrap(self._base_players,j), i % self._snd_chnls, wrap(mul,j), wrap(add,j)))
+
+    def __dir__(self):
+        return ['speed', 'mark', 'interp', 'mul', 'add']
+
+    def __del__(self):
+        for obj in self._base_objs:
+            obj.deleteStream()
+            del obj
+        for obj in self._base_players:
+            obj.deleteStream()
+            del obj
+
+    def play(self):
+        self._base_players = [obj.play() for obj in self._base_players]
+        self._base_objs = [obj.play() for obj in self._base_objs]
+        return self
+
+    def out(self, chnl=0, inc=1):
+        self._base_players = [obj.play() for obj in self._base_players]
+        if type(chnl) == ListType:
+            self._base_objs = [obj.out(wrap(chnl,i)) for i, obj in enumerate(self._base_objs)]
+        else:
+            if chnl < 0:    
+                self._base_objs = [obj.out(i*inc) for i, obj in enumerate(random.sample(self._base_objs, len(self._base_objs)))]
+            else:   
+                self._base_objs = [obj.out(chnl+i*inc) for i, obj in enumerate(self._base_objs)]
+        return self
+
+    def stop(self):
+        [obj.stop() for obj in self._base_players]
+        [obj.stop() for obj in self._base_objs]
+        return self
+
+    def setSpeed(self, x):
+        """
+        Replace the `speed` attribute.
+
+        Parameters:
+
+        x : float or PyoObject
+            new `speed` attribute.
+
+        """
+        self._speed = x
+        x, lmax = convertArgsToLists(x)
+        [obj.setSpeed(wrap(x,i)) for i, obj in enumerate(self._base_players)]
+
+    def setMark(self, x):
+        """
+        Replace the `mark` attribute.
+
+        Parameters:
+
+        x : float or PyoObject
+            new `mark` attribute.
+
+        """
+        self._mark = x
+        x, lmax = convertArgsToLists(x)
+        [obj.setMark(wrap(x,i)) for i, obj in enumerate(self._base_players)]
+
+    def setInterp(self, x):
+        """
+        Replace the `interp` attribute.
+
+        Parameters:
+
+        x : int {1, 2, 3, 4}
+            new `interp` attribute.
+
+        """
+        self._interp = x
+        x, lmax = convertArgsToLists(x)
+        [obj.setInterp(wrap(x,i)) for i, obj in enumerate(self._base_players)]
+
+    def getMarkers(self):
+        """
+        Returns a list of marker time values in samples.
+
+        """
+        return self._markers
+
+    def ctrl(self, map_list=None, title=None):
+        self._map_list = [SLMap(0.01, 2., 'lin', 'speed', self._speed), 
+                          SLMap(0, len(self._markers)-1, 'lin', 'mark', self._mark, 'int'),
+                          SLMapMul(self._mul)]
+        PyoObject.ctrl(self, map_list, title)
+
+    @property
+    def speed(self): 
+        """float or PyoObject. Transposition factor."""
+        return self._speed
+    @speed.setter
+    def speed(self, x): self.setSpeed(x)
+
+    @property
+    def mark(self): 
+        """float or PyoObject. Marker to loop."""
+        return self._marker
+    @mark.setter
+    def mark(self, x): self.setMark(x)
+
+    @property
+    def interp(self): 
+        """int {1, 2, 3, 4}. Interpolation method."""
+        return self._interp
+    @interp.setter
+    def interp(self, x): self.setInterp(x)
