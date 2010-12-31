1dc34cbcd1a1d0a46d666c361274cd08ac1e407b
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2010-12-31 01:31:03 +0000

    Added new object : Mixer

--- trunk/pyolib/pan.py
+++ trunk/pyolib/pan.py
@@ -24,6 +24,7 @@ along with pyo.  If not, see <http://www.gnu.org/licenses/>.
 """
 from _core import *
 from _maps import *
+from types import SliceType
 
 class Pan(PyoObject):
     """
@@ -570,3 +571,185 @@ class Selector(PyoObject):
     def voice(self): return self._voice
     @voice.setter
     def voice(self, x): self.setVoice(x)
+
+class Mixer(PyoObject):
+    """
+    Audio mixer.
+
+    Mixer mixes multiple inputs to an arbitrary number of outputs 
+    with independant amplitude values per mixing channel and a 
+    user defined portamento applied on amplitude changes.
+
+    Parent class: PyoObject
+
+    Parameters:
+
+    outs : int, optional
+        Number of outputs of the mixer. Available at initialization
+        time only. Defaults to 2.
+    chnls : int, optional
+        Number of channels per output. Available at initialization
+        time only. Defaults to 1.
+    time : float, optional
+        Duration, in seconds, of a portamento applied on
+        a new amplitude value for a mixing channel.
+        Defaults to 0.025.
+
+    Methods:
+
+    setTime(x) : Sets the portamento duration in seconds.
+    addInput(voice, input) : Adds a new input to the mixer.
+    delInput(voice) : Removes an input from the mixer.
+    setAmp(vin, vout, amp) : Sets the amplitude of a mixing channel.
+
+    Attributes:
+
+    time : float. Portamento applied on amplitude changes.
+
+    Notes:
+
+    User can retrieve each of the output channels by calling the Mixer
+    object with the desired channel between square brackets (see example).
+
+    Examples:
+
+    >>> s = Server().boot()
+    >>> s.start()
+    >>> a = SfPlayer(SNDS_PATH+"/transparent.aif", loop=True, mul=.2)
+    >>> b = FM(carrier=200, ratio=[.5013,.4998], index=6, mul=.2)
+    >>> mm = Mixer(outs=3, chnls=2, time=.025)
+    >>> fx1 = Disto(mm[0], drive=.9, slope=.9, mul=.2).out()
+    >>> fx2 = Freeverb(mm[1], size=.8, damp=.8).out()
+    >>> fx3 = Harmonizer(mm[2], transpo=1, feedback=.75).out()
+    >>> mm.addInput(0, a)
+    >>> mm.addInput(1, b)
+    >>> mm.setAmp(0,0,.5)
+    >>> mm.setAmp(0,1,.5)
+    >>> mm.setAmp(1,2,.5)
+    >>> mm.setAmp(1,1,.5)
+
+    """
+    def __init__(self, outs=2, chnls=1, time=0.025, mul=1, add=0):
+        PyoObject.__init__(self)
+        self._outs = outs
+        self._chnls = chnls
+        self._time = time
+        self._mul = mul
+        self._add = add
+        self._inputs = {}
+        time, mul, add, lmax = convertArgsToLists(time, mul, add)
+        self._base_players = [Mixer_base(outs, wrap(time,i)) for i in range(chnls)]
+        self._base_objs = [MixerVoice_base(self._base_players[j], i, wrap(mul,i), wrap(add,i)) for i in range(outs) for j in range(chnls)]
+        
+    def __dir__(self):
+        return ["time", "mul", "add"]
+
+    def __del__(self):
+        for obj in self._base_objs:
+            obj.deleteStream()
+            del obj
+        for obj in self._base_players:
+            obj.deleteStream()
+            del obj
+
+    def __getitem__(self, x):
+        if type(x) == SliceType:
+            return [self._base_objs[j*self._chnls+i] for j in range(x.start or 0, x.stop or sys.maxint, x.step or 1) for i in range(self._chnls)]
+        elif x < len(self._base_objs):
+            return [self._base_objs[x*self._chnls+i] for i in range(self._chnls)]
+        else:
+            print "'x' too large!"         
+
+    def play(self, dur=0, delay=0):
+        dur, delay, lmax = convertArgsToLists(dur, delay)
+        self._base_players = [obj.play(wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(self._base_players)]
+        self._base_objs = [obj.play(wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(self._base_objs)]
+        return self
+
+    def out(self, chnl=0, inc=1, dur=0, delay=0):
+        dur, delay, lmax = convertArgsToLists(dur, delay)
+        self._base_players = [obj.play(wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(self._base_players)]
+        if type(chnl) == ListType:
+            self._base_objs = [obj.out(wrap(chnl,i), wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(self._base_objs)]
+        else:
+            if chnl < 0:
+                self._base_objs = [obj.out(i*inc, wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(random.sample(self._base_objs, len(self._base_objs)))]
+            else:
+                self._base_objs = [obj.out(chnl+i*inc, wrap(dur,i), wrap(delay,i)) for i, obj in enumerate(self._base_objs)]
+        return self
+
+    def stop(self):
+        [obj.stop() for obj in self._base_players]
+        [obj.stop() for obj in self._base_objs]
+        return self
+
+    def setTime(self, x):
+        """
+        Sets the portamento duration in seconds.
+
+        Parameters:
+
+        x : float
+            New portamento duration.
+
+        """
+        self._time = x
+        x, lmax = convertArgsToLists(x)
+        [obj.setTime(wrap(x,i)) for i, obj in enumerate(self._base_players)]
+
+    def addInput(self, voice, input):
+        """
+        Adds an audio object in the mixer's inputs.
+
+        Parameters:
+
+        voice : int or string
+            Key in the mixer dictionary for this input.
+        input : PyoObject
+            Audio object to add to the mixer.
+
+        """
+        self._inputs[voice] = input
+        [obj.addInput(str(voice), wrap(input,i)) for i, obj in enumerate(self._base_players)]
+
+    def delInput(self, voice):
+        """
+        Removes an audio object from the mixer's inputs.
+
+        Parameters:
+
+        voice : int or string
+            Key in the mixer dictionary assigned to the input to remove.
+
+        """
+        if self._inputs.has_key(voice):
+            del self._inputs[voice]
+            [obj.delInput(str(voice)) for i, obj in enumerate(self._base_players)]
+
+    def setAmp(self, vin, vout, amp):
+        """
+        Sets the amplitude of a mixing channel.
+
+        Parameters:
+
+        vin : int or string
+            Key in the mixer dictionary of the desired input.
+        vout : int
+            Ouput channel where to send the signal.
+        amp : float
+            Amplitude value for this mixing channel.
+
+        """
+        if self._inputs.has_key(vin) and vout < self._outs:
+            [obj.setAmp(str(vin), vout, amp) for i, obj in enumerate(self._base_players)]
+
+    def ctrl(self, map_list=None, title=None, wxnoserver=False):
+        self._map_list = [SLMapMul(self._mul)]
+        PyoObject.ctrl(self, map_list, title, wxnoserver)
+
+    @property
+    def time(self):
+        """float. Portamento.""" 
+        return self._time
+    @time.setter
+    def time(self, x): self.setTime(x)
