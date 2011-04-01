cb22b8781e1dae808dbd312c5047eec2f6a5ba7c
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2011-04-01 23:43:45 +0000

    New object : LFO, a band-limited Low Frequency Oscillator with different wave shapes.

--- trunk/setup.py
+++ trunk/setup.py
@@ -52,7 +52,7 @@ files = ['pyomodule.c', 'servermodule.c', 'streammodule.c', 'dummymodule.c', 'mi
 source_files = [path + f for f in files]
 
 path = 'src/objects/'
-files = ['tablemodule.c', 'matrixmodule.c', 'oscilmodule.c', 'filtremodule.c', 'noisemodule.c', 'distomodule.c',
+files = ['lfomodule.c', 'tablemodule.c', 'matrixmodule.c', 'oscilmodule.c', 'filtremodule.c', 'noisemodule.c', 'distomodule.c',
         'inputmodule.c', 'fadermodule.c', 'midimodule.c', 'oscmodule.c', 'delaymodule.c', 'sfplayermodule.c',
         'metromodule.c', 'trigmodule.c', 'patternmodule.c', 'bandsplitmodule.c', 'hilbertmodule.c', 'panmodule.c',
         'selectmodule.c', 'freeverbmodule.c', 'granulatormodule.c', 'compressmodule.c', 'analysismodule.c',
