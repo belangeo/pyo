2666acc33658a0277158bc09d9430ac69a197af7
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2011-09-14 12:39:27 +0000

    New example : midictlscan.py

---
+++ examples/midictlscan.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Scan for Midi controller numbers. Launch this script from a terminal.
+
+"""
+from pyo import *
+import time
+
+pm_list_devices()
+
+num = input("Enter your Midi interface number : ")
+
+s = Server()
+s.setMidiInputDevice(num)
+s.boot().start()
+
+print "Play with your Midi controllers..."
+
+def pp(x): 
+    print "controller number =", x
+
+scan = CtlScan(pp, False)
+
+again = "y"
+while again == "y":
+    time.sleep(10)
+    scan.stop()
+    again = raw_input("Do you want to continue ? (y/n) : ")
+    if again == "y":
+        print "Continue..."
+        scan.play()
+    
+s.stop()
+time.sleep(1)
+exit()
+    
