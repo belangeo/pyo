b4b2ae4f9e29cc7462a6a971b967a7ae2ae9e205
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2011-11-04 19:34:53 +0000

    Snap take choice on more than one octave.

--- examples/snap_example.py
+++ examples/snap_example.py
@@ -11,8 +11,8 @@ env = CosTable([(0,0),(100,1),(500,.3),(8191,0)])
 wav = SquareTable(7)
 
 met = Metro(.125, 8).play()
-a = TrigXnoiseMidi(met, "loopseg", x1=1, x2=.2, mrange=(60,84))
-b = Snap(a, [0,2,4,5,7,9,11], scale=1)
+a = TrigXnoiseMidi(met, "loopseg", x1=1, x2=.2, mrange=(48,97))
+b = Snap(a, [0,4,7,11,13,17,21,25,29,33], scale=1)
 
 c = TrigEnv(met, env, mul=.2)
 d = Osc(wav, b, mul=c).out()
