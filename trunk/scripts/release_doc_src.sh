1f449addf251485261c934f862daee8a529fb4bb
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2012-01-04 07:20:23 +0000

    Updated OSX installer scripts.

--- trunk/scripts/release_doc_src.sh
+++ trunk/scripts/release_doc_src.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
 
-cp -R ./doc/manual ./doc/pyo_0.5.1-doc
+cp -R ./doc/manual ./doc/pyo_0.6.0-doc
 cd doc
-tar -cjvf pyo_0.5.1-doc.tar.bz2 pyo_0.5.1-doc
-rm -R pyo_0.5.1-doc
+tar -cjvf pyo_0.6.0-doc.tar.bz2 pyo_0.6.0-doc
+rm -R pyo_0.6.0-doc
 cd ..
 
-svn export . pyo_0.5.1-src
-tar -cjvf pyo_0.5.1-src.tar.bz2 pyo_0.5.1-src
-rm -R pyo_0.5.1-src
-
+svn export . pyo_0.6.0-src
+tar -cjvf pyo_0.6.0-src.tar.bz2 pyo_0.6.0-src
+rm -R pyo_0.6.0-src
