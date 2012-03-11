4b1467ed5101adb0afff0f28a5c86006a4a137d7
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2012-03-11 23:51:04 +0000

    E-Pyo display fine-tunning on OSX.

--- utils/E-Pyo.py
+++ utils/E-Pyo.py
@@ -1554,16 +1554,23 @@ class MainFrame(wx.Frame):
 
         self.SetMenuBar(self.menuBar)
 
+        if PLATFORM == "darwin":
+            sth = 17
+            cch = -1
+        else:
+            sth = 20
+            cch = 21
+
         self.status = self.CreateStatusBar()
         self.status.Bind(wx.EVT_SIZE, self.StatusOnSize)
         self.status.SetFieldsCount(3)
         self.status.SetStatusWidths([100,-1,-2])
         self.status.SetStatusText("Quick Search:", 0)
         self.field1X, field1Y = self.status.GetTextExtent("Quick Search:")
-        self.status_search = wx.TextCtrl(self.status, wx.ID_ANY, size=(150,20), style=wx.TE_PROCESS_ENTER)
+        self.status_search = wx.TextCtrl(self.status, wx.ID_ANY, size=(150,sth), style=wx.TE_PROCESS_ENTER|wx.NO_BORDER)
         self.status_search.Bind(wx.EVT_TEXT_ENTER, self.onQuickSearchEnter)
 
-        self.cc = FileSelectorCombo(self.status, size=(250, 21), style=wx.CB_READONLY)
+        self.cc = FileSelectorCombo(self.status, size=(250, cch), style=wx.CB_READONLY)
         self.tcp = TreeCtrlComboPopup()
         self.cc.SetPopupControl(self.tcp)
         self.Reposition()
@@ -1581,14 +1588,16 @@ class MainFrame(wx.Frame):
 
     def Reposition(self):
         if PLATFORM == "darwin":
-            yoff = 5
+            yoff1 = -1
+            yoff2 = -5
         else:
-            yoff = 0
+            yoff1 = 1
+            yoff2 = 0
         rect = self.status.GetFieldRect(2)
-        self.status_search.SetPosition((self.field1X+10, rect.y+1))
+        self.status_search.SetPosition((self.field1X+10, rect.y+yoff1))
         rect = self.status.GetFieldRect(2)
         if rect.x > self.field1X+160:
-            self.cc.SetPosition((rect.x, rect.y-yoff))
+            self.cc.SetPosition((rect.x, rect.y+yoff2))
 
     def StatusOnSize(self, evt):
         self.Reposition()
