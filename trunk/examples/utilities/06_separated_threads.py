286c46e2b51781f97953331ed00aaedae8eb8adb
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2012-03-21 12:52:45 +0000

    Added an example on how to launch the audio processing in a separated thread.

---
+++ trunk/examples/utilities/06_separated_threads.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Launch the audio processing in a separated thread and send events.
+
+"""
+import subprocess, time, random
+
+pipe = subprocess.Popen(["python -i play_snd.py"], shell=True, stdin=subprocess.PIPE).stdin
+
+# Wait for the Server to be ready
+time.sleep(2)
+
+# send events to the sub process
+for i in range(20):
+    snd = "../snds/snd_%d.aif" % random.randrange(1,7)
+    pipe.write("sf.path = '%s'\ndump = sf.play()\n" % snd)
+    time.sleep(random.uniform(.2, .5))
+
+# Stop the audio Server before exiting
+pipe.write("s.stop()\ntime.sleep(0.25)\n")
+pipe.close()
+
+
