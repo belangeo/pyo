c670456c2eaac538bb4eff30d56e90601f8e65a8
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2012-04-26 22:51:05 +0000

    E-Pyo Windows various fixes.

--- utils/epyo_builder_win32.py
+++ utils/epyo_builder_win32.py
@@ -7,11 +7,9 @@ shutil.copy("Tutorial_Custom_PyoObject.py", "Resources")
 shutil.copy("E-PyoIcon.ico", "Resources")
 shutil.copy("E-PyoIconDoc.ico", "Resources")
 
-os.system("svn export ../examples examples/")
-
-shutil.copytree("examples", "Resources/examples")
-shutil.copytree("snippets", "Resources/snippets")
-shutil.copytree("styles", "Resources/styles")
+os.system("svn export ../examples Resources/examples/")
+os.system("svn export snippets Resources/snippets/")
+os.system("svn export styles Resources/styles/")
 
 os.system("python ..\..\pyinstaller\Configure.py")
 os.system('python ..\..\pyinstaller\Makespec.py -F -c --icon=Resources\E-PyoIcon.ico "E-Pyo.py"')
@@ -24,4 +22,6 @@ os.remove("E-Pyo.spec")
 shutil.rmtree("build")
 shutil.rmtree("dist")
 shutil.rmtree("Resources")
-
+for f in os.listdir(os.getcwd()):
+    if f.startswith("warn") or f.startswith("logdict"):
+        os.remove(f)
