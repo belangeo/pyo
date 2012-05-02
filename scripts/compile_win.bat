7e5b91269ec67c56968ca3b7c03837589e945419
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2012-05-02 03:57:32 +0000

    Compiler batch file for Windows.

---
+++ scripts/compile_win.bat
@@ -0,0 +1,31 @@
+echo off
+
+echo *** Compile for python2.5 - single precision ***
+C:\Python25\python.exe setup.py install
+
+RMDIR /S /Q build
+
+echo *** Compile for python2.5 - double precision ***
+C:\Python25\python.exe setup.py install --use-double
+
+RMDIR /S /Q build
+
+echo *** Compile for python2.6 - single precision ***
+C:\Python26\python.exe setup.py install
+
+RMDIR /S /Q build
+
+echo *** Compile for python2.6 - double precision ***
+C:\Python26\python.exe setup.py install --use-double
+
+RMDIR /S /Q build
+
+echo *** Compile for python2.7 - single precision ***
+C:\Python27\python.exe setup.py install
+
+RMDIR /S /Q build
+
+echo *** Compile for python2.7 - double precision ***
+C:\Python27\python.exe setup.py install --use-double
+
+RMDIR /S /Q build
