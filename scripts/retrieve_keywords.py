8db649213ea4bb2c7cef7d7096ae2ae8c00c6429
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2012-05-03 03:24:33 +0000

    Upgraded version number to 0.6.2.

--- scripts/retrieve_keywords.py
+++ scripts/retrieve_keywords.py
@@ -1,7 +1,7 @@
 from pyo import *
 
-#t = "list from TextMate"
-#t2 = t.split("|")
+t = "PyoObject|PyoTableObject|example|class_args|pa_count_devices|pa_get_default_input|pa_get_default_output|pa_list_devices|pa_get_output_devices|pa_get_input_devices|pm_count_devices|pm_list_devices|pm_get_input_devices|pm_get_default_input|pm_get_output_devices|pm_get_default_output|sndinfo|midiToHz|midiToTranspo|BandSplit|Biquad|Clip|Compress|Counter|DCBlock|Delay|Disto|Dummy|Fader|Follower|Freeverb|Granulator|Hilbert|Input|InputFader|Metro|Midictl|Mix|Noise|Notein|Osc|OscReceive|OscSend|Pan|Pattern|Phasor|Pointer|Port|PyPattern|SPan|TrigEnv|TrigRand|TrigChoice|TrigFunc|Select|SfMarkerShuffler|SfPlayer|Sig|Sine|TableRec|Tone|Waveguide|LinTable|NewTable|SndTable|HannTable|HarmTable|Server|Stream|TableStream|SigTo|Map|SLMap|SLMapFreq|SLMapMul|SLMapPhase|SLMapQ|SLMapDur|SLMapPan|Clean_objects|Pulsar|TableRead|Thresh|SawTable|SquareTable|ZCross|Degrade|Lookup|ChebyTable|Adsr|Convolve|TableMorph|sampsToSec|secToSamps|Randi|Randh|Choice|RandInt|FM|Cloud|Linseg|WGVerb|Trig|CosTable|CurveTable|Score|SineLoop|OscLoop|Print|Sin|Cos|Tan|Abs|Sqrt|Log|Log2|Log10|Pow|Xnoise|XnoiseMidi|TrigXnoise|TrigXnoiseMidi|Snap|Change|PyoMatrixObject|NewMatrix|MatrixRec|MatrixPointer|TrigLinseg|Interp|SampHold|EQ|CallAfter|Switch|Selector|savefile|Compare|ExpTable|Expseg|TrigExpseg|Allpass|Allpass2|Phaser|Harmonizer|Record|Biquadx|Chorus|SfMarkerLooper|Beat|Looper|Percent|MatrixMorph|TableIndex|DataTable|Seq|TrigRandInt|SDelay|Mixer|AllpassWG|Blit|PinkNoise|CrossFM|Between|Mirror|Wrap|BrownNoise|IRWinSinc|IRAverage|IRPulse|IRFM|Rossler|Lorenz|LFO|MidiAdsr|Denorm|FreqShift|OscBank|Follower2|Gate|ParaTable|FourBand|FFT|IFFT|Atan2|CarToPol|PolToCar|FrameDelta|FrameAccum|TrigTableRec|OscDataSend|OscDataReceive|Floor|Round|ControlRec|ControlRead|reducePoints|NoteinRec|NoteinRead|CtlScan|MidiDelAdsr|Ceil|Bendin|Touchin|Programin|DBToA|AToDB|WinTable|serverCreated|serverBooted|Timer|RandDur|Vectral|Scale|Iter|MToF|distanceToSegment|rescale|Biquada|upsamp|downsamp|VoiceManager|XnoiseDur|SincTable|MToT|linToCosCurve|CentsToTranspo|TranspoToCents|VarPort|getVersion|pa_count_host_apis|pa_get_default_host_api|pa_list_host_apis|OscListReceive|Count|Urn|NextTrig"
+t2 = t.split("|")
 
 tree = OBJECTS_TREE
 l = []
@@ -20,8 +20,8 @@ l.append("Server")
 
 with open("pyo_keywords.txt", "w") as f:
     for i, word in enumerate(l):
-        #if word not in t2:
-        #    print word
+        if word not in t2:
+            print word
         if (i % 10) == 0:
             f.write("\n")
         f.write(word + " ")
