a0140335aa2621dad432ef03718d6a8de31c241a
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2012-06-12 02:48:30 +0000

    E-Pyo: Improved error report.

--- include/pyomodule.h
+++ include/pyomodule.h
@@ -537,8 +537,11 @@ extern PyTypeObject VectralType;
 /* INIT INPUT STREAM */
 #define INIT_INPUT_STREAM \
     if ( PyObject_HasAttrString((PyObject *)inputtmp, "server") == 0 ) { \
-        PySys_WriteStderr("TypeError: 'input' argument must be a PyoObject.\n"); \
-        Py_Exit(-1); \
+        PySys_WriteStderr("TypeError: \"input\" argument must be a PyoObject.\n"); \
+        if (PyInt_AsLong(PyObject_CallMethod(self->server, "getIsBooted", NULL))) { \
+            PyObject_CallMethod(self->server, "shutdown", NULL); \
+        } \
+        Py_Exit(1); \
     } \
     Py_INCREF(inputtmp); \
     Py_XDECREF(self->input); \
