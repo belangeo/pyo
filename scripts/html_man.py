b7e11e2817a6d3c87e4f87c265cae3618d75e3a1
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2013-02-18 01:59:14 +0000

    Release tag: 0.6.4.

--- scripts/html_man.py
+++ scripts/html_man.py
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with pyo.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 import os, inspect, shutil
 from types import ListType
