eb3a17f3b1b7521ffc1e1c2618c79dd3a8b293d0
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2013-04-13 14:09:47 +0000

    Release tag: 0.6.5.

--- installers/osx/release_x86_64.sh
+++ installers/osx/release_x86_64.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export PACKAGE_NAME=pyo_0.6.4_x86_64.pkg
-export DMG_DIR="pyo 0.6.4 Universal"
-export DMG_NAME="pyo_0.6.4_OSX-universal.dmg"
+export PACKAGE_NAME=pyo_0.6.5_x86_64.pkg
+export DMG_DIR="pyo 0.6.5 Universal"
+export DMG_NAME="pyo_0.6.5_OSX-universal.dmg"
 export INSTALLER_DIR=`pwd`/installer
 export PYO_MODULE_DIR=$INSTALLER_DIR/PyoModule/Package_Contents/tmp
 export SUPPORT_LIBS_DIR=$INSTALLER_DIR/SupportLibs/Package_Contents/usr/local/lib
