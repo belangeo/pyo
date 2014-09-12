f7e487f935222d1ef59cfc1cac618f9b02393f60
belangeo <belangeo@a261ba4e-c962-11de-a077-756b9c6e95db>
2014-09-12 14:58:08 +0000

    E-Pyo: Added RadioPyo to the template list.

--- utils/E-Pyo.py
+++ utils/E-Pyo.py
@@ -416,9 +416,69 @@ if __name__ == "__main__":
     mainFrame.Show()
     app.MainLoop()
 '''
-TEMPLATE_NAMES = {93: "Header", 94: "Pyo", 95: "WxPython", 96: "Cecilia5", 97: "Zyne", 98: "Audio Interface"}
+
+RADIOPYO_TEMPLATE = '''#!/usr/bin/env python
+# encoding: utf-8
+"""
+Template for a RadioPyo song (version 1.0).
+
+A RadioPyo song is a musical python script using the python-pyo 
+module to create the audio processing chain. You can connect to
+the radio here : http://radiopyo.acaia.ca/ 
+
+There is only a few rules:
+    1 - It must be a one-page script.
+    2 - No soundfile, only synthesis.
+    3 - The script must be finite in time, with fade-in and fade-out 
+        to avoid clicks between pieces. Use the DURATION variable.
+
+belangeo - 2014
+
+"""
+from pyo import *
+
+################### USER-DEFINED VARIABLES ###################
+### READY is used to manage the server behaviour depending ###
+### of the context. Set this variable to True when the     ###
+### music is ready for the radio. TITLE and ARTIST are the ###
+### infos shown by the radio player. DURATION set the      ###
+### duration of the audio file generated for the streaming.###
+##############################################################
+READY = False           # Set to True when ready for the radio
+TITLE = "Song Title"    # The title of the music
+ARTIST = "Artist Name"  # Your artist name
+DURATION = 300          # The duration of the music in seconds
+##################### These are optional #####################
+GENRE = "Electronic"    # Kind of your music, if there is any
+DATE = 2014             # Year of creation
+
+####################### SERVER CREATION ######################
+if READY:
+    s = Server(duplex=0, audio="offline").boot()
+    s.recordOptions(dur=DURATION, filename="radiopyo.ogg", fileformat=7)
+else:
+    s = Server(duplex=0).boot()
+
+
+##################### PROCESSING SECTION #####################
+# global volume (should be used to control the overall sound)
+fade = Fader(fadein=0.001, fadeout=10, dur=DURATION).play()
+
+
+###
+### Insert your algorithms here...
+###
+
+
+#################### START THE PROCESSING ###################
+s.start()
+if not READY:
+    s.gui(locals())
+'''
+
+TEMPLATE_NAMES = {93: "Header", 94: "Pyo", 95: "WxPython", 96: "Cecilia5", 97: "Zyne", 98: "Audio Interface", 99: "RadioPyo"}
 TEMPLATE_DICT = {93: HEADER_TEMPLATE, 94: PYO_TEMPLATE, 95: WXPYTHON_TEMPLATE, 96: CECILIA5_TEMPLATE, 
-                97: ZYNE_TEMPLATE, 98: AUDIO_INTERFACE_TEMPLATE}
+                97: ZYNE_TEMPLATE, 98: AUDIO_INTERFACE_TEMPLATE, 99: RADIOPYO_TEMPLATE}
 
 ################## BUILTIN KEYWORDS COMPLETION ##################
 FROM_COMP = ''' `module` import `*`
