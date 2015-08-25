1f0dc1aa93408f856606072c435e63fe15db4f9f
Olivier Belanger <belangeo@gmail.com>
2015-08-25 14:16:03 -0400

    Use thread instead of process for the midi listener.

--- pyolib/listener.py
+++ pyolib/listener.py
@@ -1,26 +1,29 @@
 from _core import *
-from multiprocessing import Process, Array
-import time, threading
+import time
+import threading
 
-class MidiListener(Process):
+class MidiListener(threading.Thread):
     """
-    Self-contained midi listener process.
+    Self-contained midi listener thread.
 
     This object allows to setup a Midi server that is independent
     of the audio server (mainly to be able to receive Midi data even
     when the audio server is stopped). Although it runs in a separated
-    process, the same device can't be used by this object and the audio
+    thread, the same device can't be used by this object and the audio
     server at the same time. It is adviced to call the deactivateMidi() 
     method on the audio server to avoid conflicts.
 
-    :Parent: multiprocessing.Process
+    :Parent: threading.Thread
 
     :Args:
 
         function : Python function (can't be a list)
             Function that will be called when a new midi event is available. 
-            This function is called without arguments. The Midi data is 
-            stored in the array returned by the getValueArray() method.
+            This function is called with the incoming midi data as 
+            arguments. The signature of the function must be:
+                
+            def myfunc(status, data1, data2)
+
         mididev : int, optional
             Sets the midi input device (see `pm_list_devices()` for the 
             available devices). The default, -1, means the system default
@@ -29,42 +32,22 @@ class MidiListener(Process):
 
     >>> s = Server().boot()
     >>> s.deactivateMidi()
-    >>> def midicall():
-    ...     print value[0], value[1], value[2]
+    >>> def midicall(status, data1, data2):
+    ...     print status, data1, data2
     >>> listen = MidiListener(midicall, 5)
-    >>> value = listen.getValueArray()
     >>> listen.start()
 
     """
     def __init__(self, function, mididev=-1):
-        Process.__init__(self)
+        threading.Thread.__init__(self)
         self.daemon = True
-        self.value = Array("i", [0,0,0])
         self._function = WeakMethod(function)
         self._mididev = mididev
-        self._listener = MidiListener_base(self._midirecv, self._mididev)
-
-    def getValueArray(self):
-        """
-        Returns the array containing midi values.
-        
-        This array must be kept in the main thread for inspection when
-        the function is called from the process. Stored values are:
-            
-        array[0] = status byte
-        array[1] = first data byte
-        array[2] = second data byte
-
-        """
-        return self.value
-
-    def _midirecv(self, status, data1, data2):
-        self.value[0], self.value[1], self.value[2] = status, data1, data2
-        self._function()
+        self._listener = MidiListener_base(self._function, self._mididev)
         
     def run(self):
         """
-        Starts the process. The process runs as daemon, so no need to stop it.
+        Starts the process. The thread runs as daemon, so no need to stop it.
         
         """
         self._listener.play()
@@ -87,7 +70,7 @@ class OscListener(threading.Thread):
 
         function : Python function (can't be a list)
             Function that will be called when a new OSC event is available. 
-            This function is called the incoming address and values as 
+            This function is called with the incoming address and values as 
             arguments. The signature of the function must be:
                 
             def myfunc(address, *args)
