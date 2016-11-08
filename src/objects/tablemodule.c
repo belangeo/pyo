c9b3f6d53277a6436361f2be1d04e3ddc2b19d96
Olivier Belanger <belangeo@gmail.com>
2016-11-07 21:23:35 -0500

    Filled time buffer of TableRec with 0s at the beginning of a recording.

--- src/objects/tablemodule.c
+++ src/objects/tablemodule.c
@@ -5870,6 +5870,10 @@ static PyObject * TableRec_getTriggerStream(TableRec* self) { GET_TRIGGER_STREAM
 
 static PyObject * TableRec_play(TableRec *self, PyObject *args, PyObject *kwds)
 {
+    int i;
+    for (i=0; i<self->bufsize; i++) {
+        self->time_buffer_streams[i] = 0;
+    }
     self->pointer = 0;
     self->active = 1;
     PLAY
