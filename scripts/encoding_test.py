45335e17af87b3d7e087ec229d4526b1e1fd106a
Olivier Belanger <belangeo@gmail.com>
2017-06-05 12:09:30 -0400

    Test encoding fix on linux.

--- scripts/encoding_test.py
+++ scripts/encoding_test.py
@@ -1,36 +1,33 @@
 # -*- encoding: mbcs -*-
+# The encoding line is for E-Pyo tempfile only.
+# sys.getfilesystemencoding() should be used to set
+# the encoding line added by E-Pyo.
 
 import os
 import sys
 import locale
-from pyo64 import *
-
-# sys.getfilesystemencoding() should be used to set the encoding line added by E-Pyo.
+from pyo import *
 
 print("Default encoding: ", sys.getdefaultencoding())
 print("File system encoding: ", sys.getfilesystemencoding())
 print("Locale preferred encoding: ", locale.getpreferredencoding())
 print("Locale default encoding: ", locale.getdefaultlocale())
 
+# need a python layer to encode the path in python 3.
+
 # Python 3
-p = 'bébêtte/noise.aif'.encode("mbcs")
+p = 'bÃ©bÃªtte/noise.aif'.encode(sys.getfilesystemencoding())
 
 # Python 2
-#p = 'bébêtte/noise.aif'
+#p = 'bÃ©bÃªtte/noise.aif'
 
-# sndinfo, upsamp, downsamp, savefile, savefileFromTable 
-# need a python layer to encode the path in python 3.
 info = sndinfo(p)
 print(info)
 
 s = Server().boot()
-
 sf = SfPlayer(p, mul=0.1).out()
-
 s.gui(locals())
 
 # Objects with file input #
-# Sf_family
-# CvlVerb
-# SndTable
-# PyoObject.save methods...
+# Sf_family, CvlVerb, SndTable, PyoObject.save methods...
+# sndinfo, upsamp, downsamp, savefile, savefileFromTable 
