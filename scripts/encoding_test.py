9f0552338ea931a659068db41477b720dd699632
Olivier Belanger <belangeo@gmail.com>
2017-06-05 13:16:13 -0400

    Test encoding fix on OSX.

--- scripts/encoding_test.py
+++ scripts/encoding_test.py
@@ -1,7 +1,7 @@
-# -*- encoding: mbcs -*-
-# The encoding line is for E-Pyo tempfile only.
-# sys.getfilesystemencoding() should be used to set
-# the encoding line added by E-Pyo.
+# -*- encoding: utf-8 -*-
+### The encoding line is for E-Pyo tempfile only.
+### sys.getfilesystemencoding() should be used to set
+### the encoding line added by E-Pyo.
 
 import os
 import sys
@@ -13,7 +13,7 @@ print("File system encoding: ", sys.getfilesystemencoding())
 print("Locale preferred encoding: ", locale.getpreferredencoding())
 print("Locale default encoding: ", locale.getdefaultlocale())
 
-# need a python layer to encode the path in python 3.
+### Need a python layer to encode the path in python 3.
 
 # Python 3
 p = 'bébêtte/noise.aif'.encode(sys.getfilesystemencoding())
@@ -28,6 +28,20 @@ s = Server().boot()
 sf = SfPlayer(p, mul=0.1).out()
 s.gui(locals())
 
-# Objects with file input #
-# Sf_family, CvlVerb, SndTable, PyoObject.save methods...
-# sndinfo, upsamp, downsamp, savefile, savefileFromTable 
+"""
+1 - Adapt encoding line for E-Pyo tempfile.
+
+2 - Use widestring in C layer.
+
+3 - "if sys.version > 2" -> path.encode(sys.getfilesystemencoding())
+    For python2, don't do anything.
+ 
+Objects with file input:
+Sf_family, CvlVerb, SndTable, ControlRead, ControlRec
+NoteinRead, NoteinRec, Record
+sndinfo, upsamp, downsamp, savefile, savefileFromTable
+Server: recordOptions, recstart
+PyoTableObject: save, write, read  
+PyoMatrixObject: write, read  
+
+"""
