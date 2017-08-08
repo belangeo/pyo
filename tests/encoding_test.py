a79cc389cc49f4665fd265815df01dfb09ebcd39
Olivier Belanger <belangeo@gmail.com>
2017-08-08 09:04:40 -0400

    Fixed encoding path for SfMarkerShuffler and SfMarkerLooper.

--- tests/encoding_test.py
+++ tests/encoding_test.py
@@ -1,10 +1,9 @@
+#! -*- encoding: utf-8 -*-
 import os
 import sys
 import locale
 from random import uniform
 from pyo import *
-
-#! -*- encoding: utf-8 -*-
 
 ### The encoding line is for E-Pyo tempfile only.
 ### sys.getfilesystemencoding() should be used to set
@@ -19,54 +18,62 @@ s = Server().boot()
 
 ### Need a python layer to encode the path in python 3.
 # Python 3
-#p = 'bébêtte/noise.aif'.encode(sys.getfilesystemencoding())
+#p = 'bÃ©bÃªtte/noise.aif'.encode(sys.getfilesystemencoding())
 # Python 2
-p = 'bébêtte/noise.aif'
+p = 'bÃ©bÃªtte/noise.aif'
 
 ########## SNDINFO ###############
 info = sndinfo(p)
 print("sndinfo output:\n", info)
 
 ####### SAVEFILE ##############
-sr, dur, chnls, path = 44100, 5, 2, os.path.join("bébêtte", 'savefile.aif')
+sr, dur, chnls, path = 44100, 5, 2, os.path.join("bÃ©bÃªtte", 'savefile.aif')
 samples = [[uniform(-0.5,0.5) for i in range(sr*dur)] for i in range(chnls)]
 savefile(samples=samples, path=path, sr=sr, channels=chnls, fileformat=1, sampletype=1)
-print("Savefile record:", os.path.isfile(os.path.join("bébêtte", 'savefile.aif')))
+print("Savefile record:", os.path.isfile(os.path.join("bÃ©bÃªtte", 'savefile.aif')))
 
 ####### SAVEFILEFROMTABLE #########
 home = os.path.expanduser('~')
 path1 = SNDS_PATH + '/transparent.aif'
-path2 = os.path.join("bébêtte", 'savefileFromTable.aif')
+path2 = os.path.join("bÃ©bÃªtte", 'savefileFromTable.aif')
 t = SndTable(path1)
 savefileFromTable(table=t, path=path2, fileformat=1, sampletype=1)
-print("SavefileFromTable record:", os.path.isfile(os.path.join("bébêtte", 'savefileFromTable.aif')))
+print("SavefileFromTable record:", os.path.isfile(os.path.join("bÃ©bÃªtte", 'savefileFromTable.aif')))
 
 ##### UPSAMP/DOWNSAMP ######
 # upsample a signal 3 times
-upfile = os.path.join("bébêtte", 'upsamp.aif')
+upfile = os.path.join("bÃ©bÃªtte", 'upsamp.aif')
 upsamp(p, upfile, 2, 256)
-print("upsamp record:", os.path.isfile(os.path.join("bébêtte", 'upsamp.aif')))
+print("upsamp record:", os.path.isfile(os.path.join("bÃ©bÃªtte", 'upsamp.aif')))
 # downsample the upsampled signal 3 times
-downfile = os.path.join("bébêtte", 'downsamp.aif')
+downfile = os.path.join("bÃ©bÃªtte", 'downsamp.aif')
 downsamp(upfile, downfile, 3, 256)
-print("downsamp record:", os.path.isfile(os.path.join("bébêtte", 'downsamp.aif')))
+print("downsamp record:", os.path.isfile(os.path.join("bÃ©bÃªtte", 'downsamp.aif')))
 
-sf = SfPlayer(p, mul=0.1).out()
-sf.setPath(downfile)
+######### SfPlayer ###########
+sf1 = SfPlayer(p, mul=0.1)
+sf1.setPath(downfile)
 
-######### SfMarker ###########
-#sf = SfMarkerLooper('bébêtte/transparent.aif', mul=0.3).out()
+p2 = os.path.join("bÃ©bÃªtte", 'transparent.aif')
+######### SfMarkerShuffler ###########
+sf2 = SfMarkerShuffler(p2, mul=0.3).out()
+
+######### SfMarkerLooper ###########
+sf3 = SfMarkerLooper(p2, mul=.3).out()
+rnd = RandInt(len(sf3.getMarkers()), 2)
+sf3.mark = rnd
 
 s.gui(locals(), exit=False)
 
-if os.path.isfile(os.path.join("bébêtte", 'savefile.aif')):
-    os.remove(os.path.join("bébêtte", 'savefile.aif'))
-if os.path.isfile(os.path.join("bébêtte", 'savefileFromTable.aif')):
-    os.remove(os.path.join("bébêtte", 'savefileFromTable.aif'))
-if os.path.isfile(os.path.join("bébêtte", 'upsamp.aif')):
-    os.remove(os.path.join("bébêtte", 'upsamp.aif'))
-if os.path.isfile(os.path.join("bébêtte", 'downsamp.aif')):
-    os.remove(os.path.join("bébêtte", 'downsamp.aif'))
+def delfile(f):
+    if os.path.isfile(f):
+        os.remove(f)
+
+# On windows, we should delete Sf* objects before deleting the audio files.
+delfile(os.path.join("bÃ©bÃªtte", 'savefile.aif'))
+delfile(os.path.join("bÃ©bÃªtte", 'savefileFromTable.aif'))
+delfile(os.path.join("bÃ©bÃªtte", 'upsamp.aif'))
+delfile(os.path.join("bÃ©bÃªtte", 'downsamp.aif'))
 
 """
 1 - Adapt encoding line for E-Pyo tempfile. **done**
@@ -82,6 +89,10 @@ Sf_family, CvlVerb, SndTable, ControlRead, ControlRec, Expr
 NoteinRead, NoteinRec, Record
 Server: recordOptions, recstart
 PyoTableObject: save, write, read  
-PyoMatrixObject: write, read  
+PyoMatrixObject: write, read
+
+4 - For method of a C class, use stringencode(st) before the call and
+    PyArg_ParseTuple(args, "s#", &self->path, &psize) in the method body.
+    METH_VARARGS must be used as the method flag.
 
 """
