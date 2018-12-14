fb861dd0276bc332d34b9e6095e25bd0a76f2ffa
Olivier Belanger <belangeo@gmail.com>
2018-12-14 10:47:30 -0500

    Updated wheels builder on MacOS.

--- scripts/release_wheels_OSX.sh
+++ scripts/release_wheels_OSX.sh
@@ -13,12 +13,12 @@ replace=XXX
 sudo rm -rf build dist
 
 #### Source distribution.
-#sudo /usr/local/bin/python3.7 setup.py sdist
+sudo /usr/local/bin/python3.7 setup.py sdist
 
 #### Prepare support libraries.
 mkdir temp_libs
 
-sudo cp /usr/local/lib/liblo.dylib temp_libs/liblo.dylib
+sudo cp /usr/local/lib/liblo.7.dylib temp_libs/liblo.7.dylib
 sudo cp /usr/local/lib/libportaudio.2.dylib temp_libs/libportaudio.2.dylib
 sudo cp /usr/local/lib/libportmidi.dylib temp_libs/libportmidi.dylib
 sudo cp /usr/local/lib/libsndfile.1.dylib temp_libs/libsndfile.1.dylib
@@ -32,18 +32,22 @@ sudo install_name_tool -change /usr/local/opt/flac/lib/libFLAC.8.dylib @loader_p
 sudo install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @loader_path/libogg.0.dylib libsndfile.1.dylib
 sudo install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbis.0.dylib @loader_path/libvorbis.0.dylib libsndfile.1.dylib
 sudo install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbisenc.2.dylib @loader_path/libvorbisenc.2.dylib libsndfile.1.dylib
-sudo install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @loader_path/libogg.0.dylib libvorbis.0.dylib
-sudo install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @loader_path/libogg.0.dylib libvorbisenc.2.dylib
-sudo install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @loader_path/libogg.0.dylib libFLAC.8.dylib
-# Make sure libvorbis version is correct in this path!
-sudo install_name_tool -change /usr/local/Cellar/libvorbis/1.3.6/lib/libvorbis.0.dylib @loader_path/libvorbis.0.dylib libvorbisenc.2.dylib
+#sudo install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @loader_path/libogg.0.dylib libvorbis.0.dylib
+sudo install_name_tool -change /usr/local/lib/libogg.0.dylib @loader_path/libogg.0.dylib libvorbis.0.dylib
+#sudo install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @loader_path/libogg.0.dylib libvorbisenc.2.dylib
+sudo install_name_tool -change /usr/local/lib/libogg.0.dylib @loader_path/libogg.0.dylib libvorbisenc.2.dylib
+sudo install_name_tool -change /usr/local/lib/libvorbis.0.dylib @loader_path/libvorbis.0.dylib libvorbisenc.2.dylib
+#sudo install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @loader_path/libogg.0.dylib libFLAC.8.dylib
+
+### Make sure libvorbis version is correct in this path!
+#sudo install_name_tool -change /usr/local/Cellar/libvorbis/1.3.6/lib/libvorbis.0.dylib @loader_path/libvorbis.0.dylib libvorbisenc.2.dylib
 
 cd ..
 
 ### Build pyo for python 2.7
 sudo python2 setup.py bdist_wheel --use-coreaudio --use-double
 
-wheel_file=pyo-XXX-cp27-cp27m-macosx_10_6_intel.whl
+wheel_file=pyo-XXX-cp27-cp27m-macosx_10_9_x86_64.whl
 dist_info=pyo-XXX.dist-info
 
 if cd dist; then
@@ -63,26 +67,25 @@ else
     echo "*** Something went wrong when building for python 2.7..."
 fi
 
-exit
 
 ### Build pyo for python 3.5
 sudo /usr/local/bin/python3.5 setup.py bdist_wheel --use-coreaudio --use-double
 
-wheel_file=pyo-XXX-cp35-cp35m-macosx_10_6_intel.whl
+wheel_file=pyo-XXX-cp35-cp35m-macosx_10_6_intelß.whl
 dist_info=pyo-XXX.dist-info
 
 if cd dist; then
     sudo unzip ${wheel_file/$replace/$version}
-    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib _pyo.cpython-35m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib _pyo64.cpython-35m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib _pyo.cpython-35m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib _pyo64.cpython-35m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib _pyo.cpython-35m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib _pyo64.cpython-35m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib _pyo.cpython-35m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib _pyo64.cpython-35m-darwin.so
-    sudo zip -r -X ${wheel_file/$replace/$version} _pyo.cpython-35m-darwin.so _pyo64.cpython-35m-darwin.so ${dist_info/$replace/$version} pyo.py pyo64.py pyolib pyodeps
-    sudo rm -rf _pyo.cpython-35m-darwin.so _pyo64.cpython-35m-darwin.so ${dist_info/$replace/$version} pyo.py pyo64.py pyolib pyodeps
+    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib pyo/_pyo.cpython-35m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib pyo/_pyo64.cpython-35m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib pyo/_pyo.cpython-35m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib pyo/_pyo64.cpython-35m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib pyo/_pyo.cpython-35m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib pyo/_pyo64.cpython-35m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib pyo/_pyo.cpython-35m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib pyo/_pyo64.cpython-35m-darwin.so
+    sudo zip -r -X ${wheel_file/$replace/$version} ${dist_info/$replace/$version} pyo pyo64
+    sudo rm -rf ${dist_info/$replace/$version} pyo pyo64
     cd ..
 else
     echo "*** Something went wrong when building for python 3.5..."
@@ -91,21 +94,21 @@ fi
 #### Build pyo for python 3.6
 sudo /usr/local/bin/python3.6 setup.py bdist_wheel --use-coreaudio --use-double
 
-wheel_file=pyo-XXX-cp36-cp36m-macosx_10_6_intel.whl
+wheel_file=pyo-XXX-cp36-cp36m-macosx_10_9_x86_64.whl
 dist_info=pyo-XXX.dist-info
 
 if cd dist; then
     sudo unzip ${wheel_file/$replace/$version}
-    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib _pyo.cpython-36m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib _pyo64.cpython-36m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib _pyo.cpython-36m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib _pyo64.cpython-36m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib _pyo.cpython-36m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib _pyo64.cpython-36m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib _pyo.cpython-36m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib _pyo64.cpython-36m-darwin.so
-    sudo zip -r -X ${wheel_file/$replace/$version} _pyo.cpython-36m-darwin.so _pyo64.cpython-36m-darwin.so ${dist_info/$replace/$version} pyo.py pyo64.py pyolib pyodeps
-    sudo rm -rf _pyo.cpython-36m-darwin.so _pyo64.cpython-36m-darwin.so ${dist_info/$replace/$version} pyo.py pyo64.py pyolib pyodeps
+    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib pyo/_pyo.cpython-36m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib pyo/_pyo64.cpython-36m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib pyo/_pyo.cpython-36m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib pyo/_pyo64.cpython-36m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib pyo/_pyo.cpython-36m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib pyo/_pyo64.cpython-36m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib pyo/_pyo.cpython-36m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib pyo/_pyo64.cpython-36m-darwin.so
+    sudo zip -r -X ${wheel_file/$replace/$version} ${dist_info/$replace/$version} pyo pyo64
+    sudo rm -rf ${dist_info/$replace/$version} pyo pyo64
     cd ..
 else
     echo "*** Something went wrong when building for python 3.6..."
@@ -114,21 +117,21 @@ fi
 ### Build pyo for python 3.7
 sudo /usr/local/bin/python3.7 setup.py bdist_wheel --use-coreaudio --use-double
 
-wheel_file=pyo-XXX-cp37-cp37m-macosx_10_6_intel.whl
+wheel_file=pyo-XXX-cp37-cp37m-macosx_10_9_x86_64.whl
 dist_info=pyo-XXX.dist-info
 
 if cd dist; then
     sudo unzip ${wheel_file/$replace/$version}
-    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib _pyo.cpython-37m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib _pyo64.cpython-37m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib _pyo.cpython-37m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib _pyo64.cpython-37m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib _pyo.cpython-37m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib _pyo64.cpython-37m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib _pyo.cpython-37m-darwin.so
-    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib _pyo64.cpython-37m-darwin.so
-    sudo zip -r -X ${wheel_file/$replace/$version} _pyo.cpython-37m-darwin.so _pyo64.cpython-37m-darwin.so ${dist_info/$replace/$version} pyo.py pyo64.py pyolib pyodeps
-    sudo rm -rf _pyo.cpython-37m-darwin.so _pyo64.cpython-37m-darwin.so ${dist_info/$replace/$version} pyo.py pyo64.py pyolib pyodeps
+    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib pyo/_pyo.cpython-37m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portmidi/lib/libportmidi.dylib @loader_path/pyodeps/libportmidi.dylib pyo/_pyo64.cpython-37m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib pyo/_pyo.cpython-37m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/pyodeps/libportaudio.2.dylib pyo/_pyo64.cpython-37m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib pyo/_pyo.cpython-37m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/liblo/lib/liblo.7.dylib @loader_path/pyodeps/liblo.7.dylib pyo/_pyo64.cpython-37m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib pyo/_pyo.cpython-37m-darwin.so
+    sudo install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/pyodeps/libsndfile.1.dylib pyo/_pyo64.cpython-37m-darwin.so
+    sudo zip -r -X ${wheel_file/$replace/$version} ${dist_info/$replace/$version} pyo pyo64
+    sudo rm -rf ${dist_info/$replace/$version} pyo pyo64
     cd ..
 else
     echo "*** Something went wrong when building for python 3.7..."
