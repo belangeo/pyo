e3f5fbfd0f9b5feb42198f7b85df162b2bdd9d7b
Olivier Belanger <belangeo@gmail.com>
2019-10-29 12:33:13 -0400

    Made 06-record-table.py example more straight forward.

--- pyo/examples/04-soundfiles/06-record-table.py
+++ pyo/examples/04-soundfiles/06-record-table.py
@@ -29,9 +29,15 @@ inp = Input([0,1])
 
 # Table recorder. Call rec.play() to start a recording, it stops
 # when the table is full. Call it multiple times to overdub.
-rec = TableRec(inp, table=t, fadetime=0.05)
+rec = TableRec(inp, table=t, fadetime=0.05).play()
 
 # Reads the content of the table in loop.
 osc = Osc(table=t, freq=t.getRate(), mul=0.5).out()
+
+def saveToDisk():
+    savefileFromTable(table=t, path=path, fileformat=0, sampletype=3)
+
+# After two seconds, the table content is saved to a file on disk.
+sv = CallAfter(saveToDisk, time=2).play()
 
 s.gui(locals())
