90c25dc54cbf3d0b20bb9cc14fe69ff537b34c8d
Olivier Belanger <belangeo@gmail.com>
2019-11-13 11:25:46 -0500

    Finished audio-mixer example.

--- pyo/examples/21-utilities/09-audio-mixer.py
+++ pyo/examples/21-utilities/09-audio-mixer.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python
-# encoding: utf-8
-from __future__ import print_function
 """
-Mixing multiple inputs to multiple outputs with fade time.
+Mixing multiple inputs to multiple outputs with fading time
+
+**09-audio-mixer.py**
+
+The Mixer object allows to route multiple inputs to multiple outputs
+with individual amplitude. A portamento time, defined with the `time`
+argument, is applied to amplitude changes to make smooth mixing
+variations.
+
+The initialization argument `outs` lets defined the number of available
+outputs and the argument `chnls` specifies how many channels per output.
+If `chnls` is 2 and a single audio stream is given to an input, the signal
+will be dupplicated to fill the two channels of the voice.
 
 """
 from pyo import *
@@ -10,43 +19,41 @@ import random
 
 s = Server(duplex=0).boot()
 
-# Inputs
-a = SfPlayer("../snds/ounkmaster.aif", loop=True, mul=.3)
-b = SfPlayer("../snds/flute.aif", loop=True, mul=.1)
-c = SfPlayer("../snds/baseballmajeur_m.aif", loop=True, mul=.2)
+# Define some input signals for the mixer.
+low = SfPlayer("../snds/flute.aif", speed=0.5, loop=True, mul=.3)
+mid = SfPlayer("../snds/flute.aif", speed=.75, loop=True, mul=.3)
+hig = SfPlayer("../snds/flute.aif", speed=1.0, loop=True, mul=.3)
 
-# 3 outputs mixer, 1 second of amplitude fade time
-mm = Mixer(outs=3, chnls=2, time=1)
+# Mixer with 3 outputs, 2 channels per voice and 0.5 second of amplitude fading time.
+mm = Mixer(outs=3, chnls=2, time=0.5)
 
-# Outputs
-fx1 = Disto(mm[0], drive=.99, slope=.85, mul=.1).out()
-fx2 = Freeverb(mm[1], size=.95, damp=.7, mul=2).out()
-fx3 = Harmonizer(mm[2], transpo=-7, feedback=.25, mul=2).out()
+# Route the mixer's outputs to some effects.
+octaveBelow = Harmonizer(mm[0], transpo=-12, feedback=0.25, add=mm[0]).out()
+fifthBelow  = Harmonizer(mm[1], transpo=-7, feedback=0.25, add=mm[1]).out()
+secondAbove = Harmonizer(mm[2], transpo=2, feedback=.25, add=mm[2]).out()
 
-# Add inputs to the mixer
-mm.addInput(voice=0, input=a)
-mm.addInput(voice=1, input=b)
-mm.addInput(voice=2, input=c)
+# Add signals as inputs to the mixer.
+mm.addInput(voice=0, input=low)
+mm.addInput(voice=1, input=mid)
+mm.addInput(voice=2, input=hig)
 
-# Static assignation of inputs to outputs with amplitude balance
-# mm.setAmp(vin=0, vout=0, amp=.15)
-# mm.setAmp(vin=0, vout=1, amp=.5)
-# mm.setAmp(vin=1, vout=0, amp=.25)
-# mm.setAmp(vin=1, vout=2, amp=.5)
-# mm.setAmp(vin=2, vout=1, amp=.35)
-# mm.setAmp(vin=2, vout=2, amp=.5)
+# Initial assignment of inputs to outputs with amplitude balance.
+mm.setAmp(vin=0, vout=0, amp=.25)
+mm.setAmp(vin=1, vout=2, amp=.25)
 
-inputs = {0: "ounkmaster", 1: "flute", 2: "baseballmajeur"}
-outputs = {0: "disto", 1: "reverb", 2: "harmonizer"}
+# Dictionaries used to print assigment messages.
+inputs = {0: "low", 1: "mid", 2: "hig"}
+outputs = {0: "Octave below", 1: "Fifth below", 2: "Second above"}
 
-# Dynamic assignation of inputs to outputs with random amplitude
+# Dynamic assignation of inputs to outputs with random amplitude.
 def assign():
     vin = random.randint(0, 2)
     vout = random.randint(0, 2)
-    amp = random.choice([0,0,.25,.33])
+    amp = random.choice([0, 0, .25, .33, .5])
     print("%s -> %s, amp = %f" % (inputs[vin], outputs[vout], amp))
     mm.setAmp(vin=vin, vout=vout, amp=amp)
 
+# Call a new assignment every 3 seconds.
 pat = Pattern(function=assign, time=3).play()
 
 s.gui(locals())
