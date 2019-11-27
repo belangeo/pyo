48cd199c16d5d80bd8c3cc89b6642fd9f3b577cd
Olivier Belanger <belangeo@gmail.com>
2019-11-27 14:50:29 -0500

    Added python 3.8 in programming languages meta data.

--- setup.py
+++ setup.py
@@ -288,6 +288,7 @@ classifiers = [
                 'Programming Language :: Python :: 3.5',
                 'Programming Language :: Python :: 3.6',
                 'Programming Language :: Python :: 3.7',
+                'Programming Language :: Python :: 3.8',
              ]
 
 setup(  name = "pyo",
