0024707407e2276f6cf2f831c0bf8372de38ecde
Olivier Belanger <belangeo@gmail.com>
2021-01-25 21:40:12 -0500

    Added python 3.9 to windows build.

--- scripts/release_wheels_Win.bat
+++ scripts/release_wheels_Win.bat
@@ -6,3 +6,4 @@ RMDIR /S /Q dist
 py -3.6 setup.py bdist_wheel --use-double
 py -3.7 setup.py bdist_wheel --use-double
 py -3.8 setup.py bdist_wheel --use-double
+py -3.9 setup.py bdist_wheel --use-double
