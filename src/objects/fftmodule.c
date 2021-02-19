e3ac5b30a61c65a2b7b4cbe1b31923de839a6baa
Olivier Belanger <belangeo@gmail.com>
2021-02-19 10:23:13 -0500

    Fixed reference counting related segfaults.

--- src/objects/fftmodule.c
+++ src/objects/fftmodule.c
@@ -1193,6 +1193,7 @@ CarToPol_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 
     INIT_INPUT_STREAM
 
+    Py_INCREF(input2tmp);
     Py_XDECREF(self->input2);
     self->input2 = input2tmp;
     input2_streamtmp = PyObject_CallMethod((PyObject *)self->input2, "_getStream", NULL);
@@ -1496,6 +1497,7 @@ PolToCar_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 
     INIT_INPUT_STREAM
 
+    Py_INCREF(input2tmp);
     Py_XDECREF(self->input2);
     self->input2 = input2tmp;
     input2_streamtmp = PyObject_CallMethod((PyObject *)self->input2, "_getStream", NULL);
--- src/objects/trigmodule.c
+++ src/objects/trigmodule.c
@@ -6129,6 +6129,7 @@ Timer_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 
     INIT_INPUT_STREAM
 
+    Py_INCREF(input2tmp);
     Py_XDECREF(self->input2);
     self->input2 = input2tmp;
     input2_streamtmp = PyObject_CallMethod((PyObject *)self->input2, "_getStream", NULL);
@@ -7165,6 +7166,7 @@ NextTrig_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
 
     INIT_INPUT_STREAM
 
+    Py_INCREF(input2tmp);
     Py_XDECREF(self->input2);
     self->input2 = input2tmp;
     input2_streamtmp = PyObject_CallMethod((PyObject *)self->input2, "_getStream", NULL);
