c726b23ed0a855b266b7236e4a8345e4ca3499ea
Olivier Belanger <belangeo@gmail.com>
2022-10-27 18:13:29 -0400

    Updated build manylinux wheel github action.

--- manylinux-pre-build-script.sh
+++ manylinux-pre-build-script.sh
@@ -7,13 +7,3 @@ make 1>/dev/null
 make install 1>/dev/null
 ldconfig
 cd ..
-
-echo ====== Build and install jack2. ======
-wget -O jack2-1.9.21.tar.gz https://github.com/jackaudio/jack2/archive/refs/tags/v1.9.21.tar.gz
-tar -xzf jack2-1.9.21.tar.gz
-cd jack2-1.9.21
-python waf configure LDFLAGS="-lstdc++" 1>/dev/null
-python waf build 1>/dev/null
-python waf install 1>/dev/null
-ldconfig
-cd ..
